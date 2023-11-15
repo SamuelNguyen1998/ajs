7225380e1bbbc6d4824d08d93e06ca866f950cdd
SamuelNguyen1998 <40987964+SamuelNguyen1998@users.noreply.github.com>
2023-11-15 09:54:41 +0700

    Update awxclient.py

--- actions/util/awxclient.py
+++ actions/util/awxclient.py
@@ -214,8 +214,8 @@ class AwxClient():
         return self.session.delete(self.baseURL+path, verify=False, headers=reqHeader)
     
     def getJobTemplateResult(self, job_template_id):
-        req_count = self.config.get("get_job_result_request_attempts_count", 20) # default: 20 attempts
-        req_interval = self.config.get("get_job_result_request_interval", 90) # default: 90 seconds
+        req_count = self.config.get("get_job_result_request_attempts_count", 360) # default: 360 attempts
+        req_interval = self.config.get("get_job_result_request_interval", 5) # default: 5 seconds
         output_json = {}
         for try_c in range(req_count):
             if try_c != 0:
